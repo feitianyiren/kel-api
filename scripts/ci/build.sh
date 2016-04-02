0967989c380b1af308ff3fd90e35d896c98dd752
Brian Rosner <brosner@eldarion.com>
2016-04-02 12:47:25 -0600

    Pinned buildpack and improved readability

--- scripts/ci/build.sh
+++ scripts/ci/build.sh
@@ -4,4 +4,11 @@ set -ev
 here=$(cd "$(dirname "${BASH_SOURCE}")"; pwd -P)
 . $here/_common.sh
 
-git archive --format=tar "$TRAVIS_COMMIT" | docker run -i -e DISABLE_COLLECTSTATIC=1 -e STACK=cedar-14 -e BUILDPACK_URL=https://github.com/heroku/heroku-buildpack-python.git $BUILDER_IMAGE - > $BUNDLE_FILE
+git archive --format=tar "$TRAVIS_COMMIT" | {
+    docker run \
+        --interactive \
+        --eenv STACK=cedar-14 \
+        --env BUILDPACK_URL=https://github.com/heroku/heroku-buildpack-python.git#v79 \
+        --env DISABLE_COLLECTSTATIC=1 \
+        $BUILDER_IMAGE -
+} > $BUNDLE_FILE
