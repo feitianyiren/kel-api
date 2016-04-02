ca385beeb0518c28d8eb0fd802097213b8e01ac8
Brian Rosner <brosner@eldarion.com>
2016-04-02 10:09:27 -0600

    Disabled collectstatic to fix build

--- scripts/ci/build.sh
+++ scripts/ci/build.sh
@@ -4,4 +4,4 @@ set -ev
 here=$(cd "$(dirname "${BASH_SOURCE}")"; pwd -P)
 . $here/_common.sh
 
-git archive --format=tar "$TRAVIS_COMMIT" | docker run -i -e STACK=cedar-14 -e BUILDPACK_URL=https://github.com/heroku/heroku-buildpack-python.git $BUILDER_IMAGE - > $BUNDLE_FILE
+git archive --format=tar "$TRAVIS_COMMIT" | docker run -i -e DISABLE_COLLECTSTATIC=1 -e STACK=cedar-14 -e BUILDPACK_URL=https://github.com/heroku/heroku-buildpack-python.git $BUILDER_IMAGE - > $BUNDLE_FILE
