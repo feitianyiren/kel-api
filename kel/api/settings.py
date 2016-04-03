161e86e3fc01b54648ab975d49caba08874829f5
Brian Rosner <brosner@eldarion.com>
2016-04-02 20:58:45 -0600

    Improved settings

--- kel/api/settings.py
+++ kel/api/settings.py
@@ -6,21 +6,31 @@ import dj_database_url
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 DEBUG = True
+
 ALLOWED_HOSTS = []
-SECRET_KEY = "changeme"
+
+SECRET_KEY = os.environ["SECRET_KEY"]
+
 ROOT_URLCONF = "kel.api.urls"
+
 WSGI_APPLICATION = "kel.api.wsgi.application"
+
 DATABASES = {
     "default": dj_database_url.config(default="postgres://localhost/kel")
 }
+
 MIDDLEWARE_CLASSES = [
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware"
 ]
+
 INSTALLED_APPS = [
     "pinax.api",
     "kel.api",
 ]
+
+AUTH_USER_MODEL = "api.User"
+
 KEL = {
     "IDENTITY_URL": os.environ["KEL_IDENTITY_URL"]
 }
