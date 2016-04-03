a1de27b35d99ea5194676f6b06868dba9b510517
Brian Rosner <brosner@eldarion.com>
2016-04-03 10:16:35 -0600

    Added correct middleware for authentication

--- kel/api/settings.py
+++ kel/api/settings.py
@@ -20,8 +20,10 @@ DATABASES = {
 }
 
 MIDDLEWARE_CLASSES = [
+    "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware"
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.auth.middleware.SessionAuthenticationMiddleware"
 ]
 
 INSTALLED_APPS = [
